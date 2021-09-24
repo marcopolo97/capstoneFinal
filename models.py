2250de0a57b4dfec18790716a33bc0cd6c32a05e
Marcus <marco97@gmail.com>
2021-09-24 17:00:07 -0400

    still testing for results

--- models.py
+++ models.py
@@ -1,7 +1,9 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 import os
+from dotenv import load_dotenv
 
+load_dotenv()
 database_path = os.environ.get('DATABASE_URL')
 db = SQLAlchemy()
 
