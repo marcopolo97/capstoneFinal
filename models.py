41f770047ce5242e36c21260b2c2fda69317abd4
Marcus <marco97@gmail.com>
2021-09-24 16:16:58 -0400

    uncommented because testing failed

--- models.py
+++ models.py
@@ -1,23 +1,18 @@
 from flask_sqlalchemy import SQLAlchemy
-#from flask_migrate import Migrate
-#import os
+from flask_migrate import Migrate
+import os
 
 db = SQLAlchemy()
 #migrate = Migrate()
 
-
-# Path for environmental variable that sets database path
-
-#database_path = os.environ.get('DATABASE_URL')
-
 # setup our database
 
-#def db_setup(app, database_path=database_path):
- #   app.config["SQLALCHEMY_DATABASE_URI"] = database_path
-  #  app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-   # db.app = app
-    #db.init_app(app)
-    #migrate.init_app(app, db)
+def db_setup(app, database_path=database_path):
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get('DATABASE_URL')
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.app = app
+    db.init_app(app)
+    migrate.init_app(app, db)
 
 
 # Models.
