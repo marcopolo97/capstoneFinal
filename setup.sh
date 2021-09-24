4a566755f2f605ed840a8b0c06701be54937149e
Marcus <marco97@gmail.com>
2021-09-24 19:02:17 -0400

    edited set up

--- setup.sh
+++ setup.sh
@@ -1,2 +1,2 @@
-export DATABASE_URL='postgres://abbnlfvrrzsaag:1b05287d5f3a3784fe1e4bd2b423f252d7426235c9fd33eb9d73c17a133d0736@ec2-23-21-4-7.compute-1.amazonaws.com:5432/detqgud0la0fmu'
+export DATABASE_URL=''
 
