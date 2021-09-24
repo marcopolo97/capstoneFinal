243b8314f1ca35f6e6120287afdc15595fefb510
Marcus <marco97@gmail.com>
2021-09-24 17:28:09 -0400

    added dotenv back in

--- models.py
+++ models.py
@@ -1,6 +1,9 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 database_path = os.environ["DATABASE_URL"]  # or other relevant config var
