18d03546828b4d56d9508e6e28675ee220648d34
Marcus <marco97@gmail.com>
2021-09-24 12:42:14 -0400

    fixed app

--- app.py
+++ app.py
@@ -3,12 +3,9 @@ from flask import Flask, request, abort, jsonify, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
-def create_app(test_config=None):
-  # create and configure the app
-  app = Flask(__name__)
-  CORS(app)
+app = Flask(__name__)
+CORS(app)
 
-  return app
 
 @app.route('/')
 def index():
