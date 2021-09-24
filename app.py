e62883244a8b4d0a9cbe2b81b978f7c8f20c91d2
Marcus <marco97@gmail.com>
2021-09-24 15:26:36 -0400

    updated models and added html files

--- app.py
+++ app.py
@@ -2,15 +2,69 @@ import os
 from flask import Flask, request, abort, jsonify, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+from models import db_setup
 
 app = Flask(__name__)
+db_setup(app)
 CORS(app)
+
+# # Models.
+
+from models import *
 
 
 @app.route('/')
 def index():
-  return render_template('pages/home.html')
+  return render_template('pages/home.html'), 200
 
+# Route to get Entrees 
+@app.route('/entrees', methods=['GET'])
+def get_entrees():
+
+  data = []
+
+  entrees = Entree.query.all()
+
+  for entree in entrees:
+    data.append({
+    'meat': entree.meat,
+    'side_1': entree.side_1,
+    'side_2': entree.side_2,
+    'price': entree.price
+  })
+
+  print(data)
+
+  return render_template('pages/entrees.html', entrees=data), 200
+
+# Route to get Desserts
+@app.route('/desserts', methods=['GET'])
+def get_desserts():
+  
+  desserts = Dessert.query.all()
+
+
+  
+  return render_template('pages/entrees.html', desserts=data), 200
+
+# Route to add a new Entree
+@app.route('/entrees', methods=['POST'])
+def add_entree():
+
+
+  return render_template('pages/entrees.html')
+
+# Route to update a Entree
+@app.route('/entrees/<int:id>',  methods=['PATCH'])
+def update_entree():
+
+  return render_template('pages/entrees.html')
+
+# Route to delete a Entree
+@app.route('/entrees/<int:id>',  methods=['DELETE'])
+def delete_entree():
+
+  return render_template('pages/entrees.html')
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
--- models.py
+++ models.py
@@ -1,4 +1,25 @@
-from app import db
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+import os
+
+db = SQLAlchemy()
+migrate = Migrate()
+
+
+# Path for environmental variable that sets database path
+
+database_path = os.environ['DATABASE_URL']
+
+# setup our database
+
+def db_setup(app, database_path=database_path):
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.app = app
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+
 # Models.
 #----------------------------------------------------------------------------#
 
