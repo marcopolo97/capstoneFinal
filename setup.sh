48a0a8713c5d67cf5c706e09e79441e1c7abfba6
Marcus <marco97@gmail.com>
2021-09-25 13:48:17 -0400

    added auth to project

--- setup.sh
+++ setup.sh
@@ -1,2 +1,7 @@
 export DATABASE_URL='postgres://xtbwrilmveyqnb:172b559c2a88cf51c07460fcd44f24bfa70d5f3f6f0cb1b4ded3476a18ed1b0a@ec2-44-194-6-121.compute-1.amazonaws.com:5432/d5t13jhigignft'
 
+export AUTH0_DOMAIN = 'dev-t6fpcq56.us.auth0.com'
+export ALGORITHMS = ['RS256']
+export API_AUDIENCE = 'food'
+export AUTH0_CLIENT_ID = V7YhOJePHmTi7UitJBrUQcFcJcWu34iw
+export AUTH0_CLIENT_SECRET = eJqC5lnWuUyBDx2DzCNrLZhZaALQpfv7IdP8eve6to1GvqCjhFu8AjXzP5p_LmvT
