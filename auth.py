23a222a16cb2347560b3b78b1b847e02a9cbd6c1
Marcus <marco97@gmail.com>
2021-09-25 15:47:37 -0400

    used direct link

--- auth.py
+++ auth.py
@@ -6,7 +6,7 @@ from urllib.request import urlopen
 import os
 
 
-AUTH0_DOMAIN = os.environ.get('AUTH0_DOMAIN')
+AUTH0_DOMAIN = 'dev-t6fpcq56.us.auth0.com'
 ALGORITHMS = ['RS256']
 API_AUDIENCE = 'food'
 
