8ab4dea4eb5756a0e52cdc40bb3cc293e7bb5ba1
Marcus <marco97@gmail.com>
2021-09-25 15:45:31 -0400

    setup updated

--- setup.sh
+++ setup.sh
@@ -1,5 +1,4 @@
 export DATABASE_URL='postgres://xtbwrilmveyqnb:172b559c2a88cf51c07460fcd44f24bfa70d5f3f6f0cb1b4ded3476a18ed1b0a@ec2-44-194-6-121.compute-1.amazonaws.com:5432/d5t13jhigignft'
-
 export AUTH0_DOMAIN='dev-t6fpcq56.us.auth0.com'
 export ALGORITHMS=['RS256']
 export API_AUDIENCE='food'
